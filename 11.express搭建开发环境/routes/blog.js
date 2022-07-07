6ebce6486ee7461830b3bb6d537c2c4bebdb47dd
node_deno <node_deno@163.com>
2022-07-07 21:33:51 +0800

    20220707-5-在express添加登录功能

--- 11.express搭建开发环境/routes/blog.js
+++ 11.express搭建开发环境/routes/blog.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const {getList, getDetail, newBlog, updateBlog, deleteBlog} = require('../controller/blog')
 const {SuccessModel, ErrorModel} = require('../model/resModel')
 
+const loginCheck = require('../middleware/loginCheck')
+
 
 //获取博客列表
 router.get('/list', function (req, res, next) {
@@ -11,16 +13,14 @@ router.get('/list', function (req, res, next) {
     let author = req.query.author || ''
     let keyword = req.query.keyword || ''
 
-    // //如果前端传的query参数中包含 isadmin 那么说明要查询作者为该用户的博客列表
-    // if (req.query.isadmin) {
-    //     let loginCheckResult = loginCheck(req)
-    //     if (loginCheckResult) {
-    //         //    未登录直接返回报错
-    //         return loginCheckResult
-    //     }
-    //     // 登录之后查询作者为自己的博客
-    //     author = req.session.username
-    // }
+    //如果前端传的query参数中包含 isadmin 那么说明要查询作者为该用户的博客列表
+    if (req.query.isadmin) {
+        if (req.session.username == null) {
+            res.json(new ErrorModel('未登录'))
+            return
+        }
+        author = req.session.username
+    }
 
     getList(author, keyword).then(data => {
         res.json(new SuccessModel(data))
@@ -29,11 +29,14 @@ router.get('/list', function (req, res, next) {
     })
 })
 
+//获取博客详情
 router.get('/detail', function (req, res, next) {
-    res.json({
-        errno: 0,
-        data: 'ok'
+    getDetail(req.query.id).then(data => {
+        res.json(new SuccessModel(data))
     })
 })
+
+//新建一篇博客
+
 
 module.exports = router
