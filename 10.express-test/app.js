b86e7bae708a2d2d7bb3fa67f66a0cf9381ff7b3
node_deno <node_deno@163.com>
2022-07-07 10:14:00 +0800

    20220707-1-使用express搭建博客开发环境

--- 10.express-test/app.js
+++ 10.express-test/app.js
@@ -1,5 +1,4 @@
-const express = require('')
-const htpp = require('http')
+const express = require('express')
 
 //本次http请求的实例
 const app = express()
@@ -22,8 +21,8 @@ app.use((req, res, next) => {
             a: 100,
             b: 200
         }
+        next()
     })
-    next()
 })
 
 app.use('/api', (req, res, next) => {
@@ -39,4 +38,41 @@ app.get('/api', (req, res, next) => {
 app.post('/api', (req, res, next) => {
     console.log('post /api 路由')
     next()
+})
+
+//模拟登录验证
+function loginCheck(req, res, next) {
+    setTimeout(() => {
+        console.log('模拟登录成功')
+        next()
+    })
+}
+
+app.get('/api/get-cookie', loginCheck, (req, res, next) => {
+    console.log('get /api/get-cookie')
+    res.json({
+        errno: 0,
+        data: req.cookie
+    })
+})
+
+app.post('/api/get-post-data', loginCheck, (req, res, next) => {
+    console.log('post /api/get-post-data')
+
+    res.json({
+        errno: 0,
+        data: req.body
+    })
+})
+
+app.use((req, res, next) => {
+    console.log('处理 404')
+    res.json({
+        errno: -1,
+        msg: '404 not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('=========项目启动了=========')
 })
