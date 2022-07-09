ab8db2892c04a88018aebc1e188b65293bdae9e7
node_deno <node_deno@163.com>
2022-07-10 00:28:23 +0800

    20220709-1-学习PM2操作

--- 12.pm2-test/index.js
+++ 12.pm2-test/index.js
@@ -3,9 +3,14 @@ let http = require('http')
 let server = http.createServer((req, res) => {
 
     //模拟日志
-    console.log('我是模拟日志信息',Date.now())
+    console.log('我是模拟日志信息', Date.now())
     //模拟错误
-    console.error('我是模拟日志错误',Date.now())
+    console.error('我是模拟日志错误', Date.now())
+
+    //模拟一个错误
+    if (req.url === '/err') {
+        throw new Error('/err出错了')
+    }
 
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({
